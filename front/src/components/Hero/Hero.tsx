56c6fbdbbd68f9f7b621f585e59506f3232f5784
vmgaray <victoriamegaray@gmail.com>
2025-04-28 08:49:40 -0300

    probando

--- front/src/components/Hero/Hero.tsx
+++ front/src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <header className="h-[12.5rem] flex items-center justify-center flex-col bg-secundant rounded-2xl my-3 #272727">
        <h1>VGB STORE</h1>
-       <Link href="/products"> Ir a productos</Link>
+       <Link href="/products"> Todos los productos</Link>
     </header>
   );  
 }
